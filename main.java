a3e2a3c4247080b76ed6810acc4990192c81f61c
Hatal24 <s224523@dtu.dk>
2024-10-11 22:58:23 +0200

    another fix #7

--- main.java
+++ main.java
@@ -134,7 +134,7 @@ class Interpreter extends AbstractParseTreeVisitor<AST>
 
 
 	public AST visitExps(ccParser.ExpsContext ctx) {
-		List<Exp> ex = new ArrayList<Exp>(ctx.e1);
+		List<Exp> ex = new ArrayList<Exp>((Exp) visit(ctx.e1));
 		for(ccParser.ExpContext i : ctx.e2) {
 			ex.add((Exp) visit(i));
 		}
